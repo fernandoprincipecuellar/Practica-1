d9dcba245432b4b5e1b3ea14c0499965fdf02539
fernandoprincipecuellar <fernandoprincipe20.a@gmail.com>
2026-03-28 13:02:59 -0500

    mejorar layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,7 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        return RedirectToAction("Index", "Campanas");
     }
 
     public IActionResult Privacy()
--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Campanas}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
